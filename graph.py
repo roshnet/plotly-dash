ed1aecc80ba7c24f127c808d07488d57da622f38
Roshan Sharma <reprogram46@gmail.com>
2019-02-24 02:41:50 +0530

    Realtime value change

--- graph.py
+++ graph.py
@@ -1,77 +1,23 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.graph_objs as go
-
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
 
-
-df = pd.read_csv('datasets/gdp-life-exp-2007.csv')
-
 app.layout = html.Div([
-		dcc.Graph(
-			id = 'life-exp-vs-gdp',
-			figure = {
-				'data': [
-					go.Scatter(
-						x = df[df['continent'] == i]['gdp per capita'],
-						y = df[df['continent'] == i]['life expectancy'],
-						text = df[df['continent'] == i]['country'],
-						mode = 'markers',
-						opacity = 0.8,
-						marker = {
-							'size': 15,
-							'line': {
-								'width': 0.5,
-								'color': 'white'
-							}
-						},
-						name = i
-					) for i in df.continent.unique()
-				],
-				'layout': go.Layout(
-					xaxis = {
-						'type': 'log',
-						'title': 'GDP per capita'
-					},
-					yaxis = {
-						'title': 'Life Expectancy'
-					},
-					margin = {
-						'l': 40,
-						'b': 40,
-						't': 10,
-						'r': 10
-					},
-					legend = {
-						'x': 0,
-						'y': 1
-					},
-					hovermode = 'closest'
-				)
-			}
-		),
-		dcc.Dropdown(
-			options = [
-				{
-					'label': 'New York City',
-					'value': 'NYC'
-				},
-				{
-					'label': 'Montreal',
-					'value': 'MTL'
-				},
-				{
-					'label': 'San Francisco',
-					'value': 'SF'
-				}
-			],
-			value = 'NYC'	# default value
-		)
-	]
+	dcc.Input(id='my-id', value='Dash App', type='text'),
+	html.Div(id='my-div')
+])
+
+
+@app.callback(
+	Output(component_id='my-div', component_property='children'),
+	[Input(component_id='my-id', component_property='value')]
 )
+def update_output_div(input_value):
+	return 'You have entered: "{}"'.format(input_value)
+
 
 if __name__ == '__main__':
 	app.run_server(debug=True)
